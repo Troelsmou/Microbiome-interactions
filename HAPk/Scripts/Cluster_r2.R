fd286ad29a70927c19aa4d80563f8bf87f0f73ea
Troelsmou <107362995+Troelsmou@users.noreply.github.com>
2024-09-17 13:16:32 +0200

    Update Cluster_r2.R

--- HAPk/Scripts/Cluster_r2.R
+++ HAPk/Scripts/Cluster_r2.R
@@ -1,3 +1,27 @@
+# Copyright (C) 2024 Troels Mouritzen
+# 
+# Author: Troels Mouritzen
+# Affiliation: Aarhus University
+# Email: twm@mbg.au.dk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Description: This script calculates HAPk scores
+# on a SNP table in non-overlapping windows along the genome
+# up to a certain amount of clusters
+# Associated publication: [PAPER TITLE], [JOURNAL], [YEAR]
+# DOI: [DOI OF PAPER]
 
 main <- function() {
   library(tidyverse)
